06c04855b8aed3c62cc500bdf722e9f677702500
euanjudd <euan.judd@hotmail.com>
2024-07-09 22:30:28 +0900

    css test v4

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -14,34 +14,34 @@ module.exports = {
         './theme/templates/*.html',
         './theme/static_src/src/**/*.js',
     ],
-    // theme: {
-    //     extend: {
-    //         colors: {
-    //           'primary-bg': '#06AB78',
-    //           'primary-text': '#FFFFFF',
-    //           'base-content': '#30313D',
-    //           'base-content-secondary': '#5C5B61',
-    //           'base-bg': '#F8F9FD',
-    //           'highlight-bg': '#3d3030',
-    //           'highlight-text': '#FFFFFF',
-    //           'border-color-default': '#30313D',
-    //         },
-    //         fontSize: {
-    //           'base': '16px',
-    //         },
-    //         fontWeight: {
-    //           'normal': '400',
-    //           'black': '900',
-    //         },
-    //         letterSpacing: {
-    //           'tight': '-0.4px',
-    //         },
-    //         lineHeight: {
-    //           'tight': '1',
-    //           'normal': '1.625',
-    //         },
-    //       },
-    // },
+    theme: {
+        extend: {
+            colors: {
+              'primary-bg': '#06AB78',
+              'primary-text': '#FFFFFF',
+              'base-content': '#30313D',
+              'base-content-secondary': '#5C5B61',
+              'base-bg': '#F8F9FD',
+              'highlight-bg': '#3d3030',
+              'highlight-text': '#FFFFFF',
+              'border-color-default': '#30313D',
+            },
+            fontSize: {
+              'base': '16px',
+            },
+            fontWeight: {
+              'normal': '400',
+              'black': '900',
+            },
+            letterSpacing: {
+              'tight': '-0.4px',
+            },
+            lineHeight: {
+              'tight': '1',
+              'normal': '1.625',
+            },
+          },
+    },
     plugins: [
         /**
          * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
